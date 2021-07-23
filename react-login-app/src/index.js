61353ead70a8448c70d818bde16c94d9cd5fb1fa
HaMinhKhoi29092000 <haminhkhoi.29092000.14@gmail.com>
2021-07-23 12:04:38 +0700

    feat : add login, logout and signin features

--- react-login-app/src/index.js
+++ react-login-app/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8082/create-web-service/'
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('tokenType') + localStorage.getItem('token');
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
