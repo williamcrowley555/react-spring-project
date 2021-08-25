5f606e2852ffe6bcbe4207269fbe6ce7f6f57f12
HaMinhKhoi29092000 <haminhkhoi.29092000.14@gmail.com>
2021-08-25 11:26:27 +0700

    add tableRepository test

--- create-web-service/src/main/java/com/william/createwebservice/io/repository/BookingRepository.java
+++ create-web-service/src/main/java/com/william/createwebservice/io/repository/BookingRepository.java
@@ -1,7 +1,11 @@
 package com.william.createwebservice.io.repository;
 
 import com.william.createwebservice.io.entity.BookingEntity;
+import com.william.createwebservice.io.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookingRepository extends JpaRepository<BookingEntity,Long> {
+    Optional<BookingEntity> findByBookingId(String bookingId);
 }
---
+++ create-web-service/src/test/java/com/william/createwebservice/io/repository/TableReponsitoryTest.java
@@ -0,0 +1,13 @@
+package com.william.createwebservice.io.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class TableReponsitoryTest {
+    @Autowired private TableRepository repo;
+}
